24501761cf822b21d7f397abf44bd0c2d61b7eb9
Ben Mordue <benmordue@gmail.com>
2014-07-18 12:16:39 +0100

    Barebones rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Littlegreenmen::Application.config.secret_key_base = '1bc225b4724bb02dca797dfe6a85befcf125d0e5e8480138a541bcf055d5229c5a96ba7cd660a3452244b4187868fa88b2cb0f97c87776807da2ca1d1de92ee5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Littlegreenmen::Application.config.session_store :cookie_store, key: '_littlegreenmen_session'
