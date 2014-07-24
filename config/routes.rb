2786e4c60914fc5e842753d2b81f1a0825ac825a
Ben Mordue <benmordue@gmail.com>
2014-07-24 21:30:49 +0100

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Littlegreenmen::Application.routes.draw do
   resources :games
 
   get 'games/:id/terrain' => 'games#terrain'
+  
+  get '/' => 'games#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
