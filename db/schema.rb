254bf40769fea8cc0e7edb143dd553910d9e859c
Ben Mordue <benmordue@gmail.com>
2014-08-04 21:58:44 +0100

    Remove superfluous turn column from games table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140801224013) do
+ActiveRecord::Schema.define(version: 20140804205708) do
 
   create_table "actors", force: true do |t|
     t.datetime "created_at"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 20140801224013) do
   end
 
   create_table "games", force: true do |t|
-    t.integer  "turn"
     t.text     "terrain"
     t.datetime "created_at"
     t.datetime "updated_at"
