c087be9ae2dd4a65480164dc824ddebcbcfce616
Ben Mordue <benmordue@gmail.com>
2014-08-08 10:58:29 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140804205708) do
+ActiveRecord::Schema.define(version: 20140805192831) do
 
   create_table "actors", force: true do |t|
     t.datetime "created_at"
@@ -25,12 +25,14 @@ ActiveRecord::Schema.define(version: 20140804205708) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "turn_id"
   end
 
   create_table "game_events", force: true do |t|
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "turn_id"
   end
 
   create_table "games", force: true do |t|
