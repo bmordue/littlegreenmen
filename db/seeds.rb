5b267229a3d41059c01e8eec13bc7f6b7bb73f17
Ben Mordue <benmordue@gmail.com>
2014-08-08 10:55:54 +0100

    Use fixtures to seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'active_record/fixtures'
+ActiveRecord::Fixtures.create_fixtures(Rails.root.join('spec/fixtures'), 'actors.yml')
+ActiveRecord::Fixtures.create_fixtures(Rails.root.join('spec/fixtures'), 'game_actions.yml')
+ActiveRecord::Fixtures.create_fixtures(Rails.root.join('spec/fixtures'), 'game_events.yml')
+ActiveRecord::Fixtures.create_fixtures(Rails.root.join('spec/fixtures'), 'games.yml')
+ActiveRecord::Fixtures.create_fixtures(Rails.root.join('spec/fixtures'), 'players.yml')
+ActiveRecord::Fixtures.create_fixtures(Rails.root.join('spec/fixtures'), 'turns.yml')
