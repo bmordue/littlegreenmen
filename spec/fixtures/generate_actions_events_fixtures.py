b24703404c9a16610f29d3a55989e32fc583bc2f
Ben Mordue <benmordue@gmail.com>
2014-08-11 20:40:11 +0100

    Re-generate fixtures

--- spec/fixtures/generate_actions_events_fixtures.py
+++ spec/fixtures/generate_actions_events_fixtures.py
@@ -19,7 +19,7 @@ for actor in range(1,6):
    for turn in range(1,4):
       event_id += 1
       f.write("ev_game1-actor" + str(actor) + "turn" + str(turn) + ":\n")
-      f.write("   id: " + str(action_id) + "\n")
+      f.write("   id: " + str(event_id) + "\n")
       f.write("   description: \"{ actor: " + str(actor) + ", moves: 'ruldruldrl' }\"\n")
       f.write("   turn_id: " + str(turn) + "\n\n")
 f.close()
