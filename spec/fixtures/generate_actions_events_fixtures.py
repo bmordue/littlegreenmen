a0d399debec32787b9947cc37ba587b757a7b01a
Ben Mordue <benmordue@gmail.com>
2015-02-01 21:42:11 +0000

    Remove ids from fixtures

--- spec/fixtures/generate_actions_events_fixtures.py
+++ spec/fixtures/generate_actions_events_fixtures.py
@@ -3,23 +3,23 @@
 # NOTE: overwrites any existing file
 
 f = open("game_actions.yml","w+")
-action_id = 0
+#action_id = 0
 for actor in range(1,6):
    for turn in range(1,4):
-      action_id += 1
+#      action_id += 1
       f.write("ac_game1-actor" + str(actor) + "turn" + str(turn) + ":\n")
-      f.write("   id: " + str(action_id) + "\n")
+#      f.write("   id: " + str(action_id) + "\n")
       f.write("   description: \"{ actor: " + str(actor) + ", moves: 'ruldruldrl' }\"\n")
-      f.write("   turn_id: " + str(turn) + "\n\n")
+      f.write("   turn: firstgameturns" + str(turn) + "\n\n")
 f.close()
 
 f = open("game_events.yml","w+")
-event_id = 0
+#event_id = 0
 for actor in range(1,6):
    for turn in range(1,4):
-      event_id += 1
+#      event_id += 1
       f.write("ev_game1-actor" + str(actor) + "turn" + str(turn) + ":\n")
-      f.write("   id: " + str(event_id) + "\n")
+#      f.write("   id: " + str(event_id) + "\n")
       f.write("   description: \"{ actor: " + str(actor) + ", moves: 'ruldruldrl' }\"\n")
-      f.write("   turn_id: " + str(turn) + "\n\n")
+      f.write("   turn: firstgameturns" + str(turn) + "\n\n")
 f.close()
--- spec/fixtures/generate_turns_fixtures.py
+++ spec/fixtures/generate_turns_fixtures.py
@@ -5,7 +5,7 @@
 f = open("turns.yml","w+")
 for x in range(1,30):
     f.write("firstgameturns" + str(x) + ":\n")
-    f.write("   id: " + str(x) + "\n")
+#    f.write("   id: " + str(x) + "\n")
     f.write("   sequence: " + str(x) + "\n")
-    f.write("   game_id: 1\n\n")
+    f.write("   game: firstgame\n\n")
 f.close()
